cbcc4d22e21c2c47fe4343753729fb027e9302f5
Rydahhhhhh <rmswerth@gmail.com>
2024-09-25 09:57:24 +1200

    Renaming variables

--- Scenes/Character Planner Menu/input_column.gd
+++ Scenes/Character Planner Menu/input_column.gd
@@ -1,24 +1,24 @@
 @tool
 extends VBoxContainer
 
-@onready var input_character_lv: LineEdit = %"Input Character Lv"
-@onready var input_b_atk_lv: LineEdit = %"Input BAtk Lv"
-@onready var input_skill_lv: LineEdit = %"Input Skill Lv"
-@onready var input_ultimate_lv: LineEdit = %"Input Ultimate Lv"
-@onready var input_talent_lv: LineEdit = %"Input Talent Lv"
+@onready var input_lv_character: LineEdit = %"InputLv Character"
+@onready var input_lv_basic_atk: LineEdit = %"InputLv BasicAtk"
+@onready var input_lv_skill: LineEdit = %"InputLv Skill"
+@onready var input_lv_ultimate: LineEdit = %"InputLv Ultimate"
+@onready var input_lv_talent: LineEdit = %"InputLv Talent"
 
-@onready var line_edit_nodes = [
-	input_character_lv, 
-	input_b_atk_lv, 
-	input_skill_lv, 
-	input_ultimate_lv, 
-	input_talent_lv
+@onready var input_lv_nodes = [
+	input_lv_character, 
+	input_lv_basic_atk, 
+	input_lv_skill, 
+	input_lv_ultimate, 
+	input_lv_talent
 ]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for line_edit_nodes in line_edit_nodes:
-		line_edit_nodes.text_changed.connect(text_input_changed.bind(line_edit_nodes))
+	for input_lv_node in input_lv_nodes:
+		input_lv_node.text_changed.connect(text_input_changed.bind(input_lv_node))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -27,29 +27,29 @@ func _process(delta: float) -> void:
 func update_data(data: Dictionary):
 	return
 
-func text_input_changed(new_text: String, line_edit_node: LineEdit):
-	validate_int_input(new_text, line_edit_node)
-	
-	var lv = line_edit_node.text as int
+func text_input_changed(new_text: String, input_lv_node: LineEdit):
+	validate_int_input(new_text, input_lv_node)
 	
 	var max = 0
-	match line_edit_node:
-		input_character_lv: max = 80
-		input_b_atk_lv: max = 6
-		input_skill_lv: max = 12
-		input_ultimate_lv: max = 12
-		input_talent_lv: max = 12
+	match input_lv_node:
+		input_lv_character: max = 80
+		input_lv_basic_atk: max = 6
+		input_lv_skill: max = 12
+		input_lv_ultimate: max = 12
+		input_lv_talent: max = 12
+	
+	var lv = input_lv_node.text as int
 	
 	if lv > max:
-		line_edit_node.text = str(max)
-		line_edit_node.caret_column = len(str(max))
+		input_lv_node.text = str(max)
+		input_lv_node.caret_column = len(str(max))
 	
 
-func validate_int_input(new_text: String, line_edit_node: LineEdit) -> void:
+func validate_int_input(new_text: String, input_lv_node: LineEdit) -> void:
 	var results = RegEx.create_from_string(r"\D+").search_all(new_text)
 	results.reverse()
 	for result in results:
-		line_edit_node.delete_text(result.get_start(), result.get_end())
+		input_lv_node.delete_text(result.get_start(), result.get_end())
 	
-	if line_edit_node.text.is_empty():
-		line_edit_node.insert_text_at_caret("1")
+	if input_lv_node.text.is_empty():
+		input_lv_node.insert_text_at_caret("1")
