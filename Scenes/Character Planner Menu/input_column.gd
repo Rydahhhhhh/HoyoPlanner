4b59485e1843f0d144e0506e7417ab6b43e8dbbb
Rydahhhhhh <rmswerth@gmail.com>
2024-09-25 10:22:38 +1200

    Apropriately renames some nodes

--- Scenes/Character Planner Menu/input_column.gd
+++ Scenes/Character Planner Menu/input_column.gd
@@ -24,8 +24,9 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func update_data(data: Dictionary):
-	return
+# ============================================================== #
+#                           TEXT INPUT                           #
+# ============================================================== #
 
 func text_input_changed(new_text: String, input_lv_node: LineEdit):
 	validate_int_input(new_text, input_lv_node)
@@ -47,9 +48,15 @@ func text_input_changed(new_text: String, input_lv_node: LineEdit):
 
 func validate_int_input(new_text: String, input_lv_node: LineEdit) -> void:
 	var results = RegEx.create_from_string(r"\D+").search_all(new_text)
+	# Avoids possible indexing issues
 	results.reverse()
+	
+	# Going to avoid settings the text directly when possible
+	# Otherwise LineEdit sets the caret_column to 0
 	for result in results:
 		input_lv_node.delete_text(result.get_start(), result.get_end())
 	
+	# Currently, you aren't able to backspace on a single character 
+	# May opt to settings this after focus is removed instead
 	if input_lv_node.text.is_empty():
 		input_lv_node.insert_text_at_caret("1")
