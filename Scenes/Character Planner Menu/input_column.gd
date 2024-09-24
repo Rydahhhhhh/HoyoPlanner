310d4cb3e6ba2097599d8071ccb16e52c3973cfe
Rydahhhhhh <rmswerth@gmail.com>
2024-09-25 09:24:57 +1200

    Validated int inputs

--- Scenes/Character Planner Menu/input_column.gd
+++ Scenes/Character Planner Menu/input_column.gd
@@ -1,9 +1,18 @@
 @tool
 extends VBoxContainer
 
+@onready var line_edit_nodes = [
+	%"Input Character Lv", 
+	%"Input BAtk Lv", 
+	%"Input Skill Lv", 
+	%"Input Ultimate Lv", 
+	%"Input Talent Lv"
+]
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	for line_edit_nodes in line_edit_nodes:
+		line_edit_nodes.text_changed.connect(text_input_changed.bind(line_edit_nodes))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -11,3 +20,12 @@ func _process(delta: float) -> void:
 
 func update_data(data: Dictionary):
 	return
+
+func text_input_changed(new_text: String, line_edit_node: LineEdit):
+	validate_int_input(new_text, line_edit_node)
+
+func validate_int_input(new_text: String, line_edit_node: LineEdit) -> void:
+	var results = RegEx.create_from_string(r"\D+").search_all(new_text)
+	results.reverse()
+	for result in results:
+		line_edit_node.delete_text(result.get_start(), result.get_end())
