42bf7282aa130e1839ecd5a520f45908b11a4ffa
Rydahhhhhh <rmswerth@gmail.com>
2024-09-25 09:49:56 +1200

    Max Level Validation

--- Scenes/Character Planner Menu/input_column.gd
+++ Scenes/Character Planner Menu/input_column.gd
@@ -1,12 +1,18 @@
 @tool
 extends VBoxContainer
 
+@onready var input_character_lv: LineEdit = %"Input Character Lv"
+@onready var input_b_atk_lv: LineEdit = %"Input BAtk Lv"
+@onready var input_skill_lv: LineEdit = %"Input Skill Lv"
+@onready var input_ultimate_lv: LineEdit = %"Input Ultimate Lv"
+@onready var input_talent_lv: LineEdit = %"Input Talent Lv"
+
 @onready var line_edit_nodes = [
-	%"Input Character Lv", 
-	%"Input BAtk Lv", 
-	%"Input Skill Lv", 
-	%"Input Ultimate Lv", 
-	%"Input Talent Lv"
+	input_character_lv, 
+	input_b_atk_lv, 
+	input_skill_lv, 
+	input_ultimate_lv, 
+	input_talent_lv
 ]
 
 # Called when the node enters the scene tree for the first time.
@@ -23,9 +29,27 @@ func update_data(data: Dictionary):
 
 func text_input_changed(new_text: String, line_edit_node: LineEdit):
 	validate_int_input(new_text, line_edit_node)
+	
+	var lv = line_edit_node.text as int
+	
+	var max = 0
+	match line_edit_node:
+		input_character_lv: max = 80
+		input_b_atk_lv: max = 6
+		input_skill_lv: max = 12
+		input_ultimate_lv: max = 12
+		input_talent_lv: max = 12
+	
+	if lv > max:
+		line_edit_node.text = str(max)
+		line_edit_node.caret_column = len(str(max))
+	
 
 func validate_int_input(new_text: String, line_edit_node: LineEdit) -> void:
 	var results = RegEx.create_from_string(r"\D+").search_all(new_text)
 	results.reverse()
 	for result in results:
 		line_edit_node.delete_text(result.get_start(), result.get_end())
+	
+	if line_edit_node.text.is_empty():
+		line_edit_node.insert_text_at_caret("1")
