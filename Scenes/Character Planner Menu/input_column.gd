769b96a8a767de11381090c1d555203db8b4732f
Rydahhhhhh <rmswerth@gmail.com>
2024-09-29 09:35:33 +1300

    Input column rewrite
    I realised I keep under utilising the editor itself, I keep trying to make scenes with code when there's literally a very powerful tool right there DESIGNED TO make scenes.

--- Scenes/Character Planner Menu/input_column.gd
+++ Scenes/Character Planner Menu/input_column.gd
@@ -1,7 +1,6 @@
 @tool
 class_name InputColumn extends VBoxContainer
 
-const INPUT_CELL = preload("res://Scenes/Character Planner Menu/InputCell.tscn")
 const InputRow = preload("res://Scenes/Character Planner Menu/input_row.gd")
 
 static var input_rows = []
@@ -15,46 +14,13 @@ var my_input_nodes = []
 func _ready() -> void:
 	add_to_group("INPUT_COLUMN")
 	input_columns.append(self)
-
-func _notification(what: int) -> void:
-	match what:
-		NOTIFICATION_PREDELETE:
-			for input_node in my_input_nodes:
-				pass
-				#input_nodes[input_node.name].erase(input_node)
-		NOTIFICATION_EDITOR_PRE_SAVE:
-			pass
-		NOTIFICATION_EDITOR_POST_SAVE:
-			pass
-
-# ====================================================== #
-#                      ROW CREATION                      #
-# ====================================================== #
-func add_cell(cell: Node, name_as: String = ""):
-	assert(not get_node_or_null(name_as))
-	add_child(cell, true)
 	
-	var x = input_rows
-	var cell_index = cell.get_index()
-	if len(input_rows) < cell_index:
-		input_rows.resize(cell_index)
-		input_rows[-1] = InputRow.new()
-	input_rows[cell_index-1].add_cell(cell)
-	
-	cell.owner = self
-	
-	if not name_as.is_empty():
-		cell.name = name_as
-	
-	return cell
-
-func add_input_cell(preset: String, preset_data: Dictionary = {}):
-	var input_cell = INPUT_CELL.instantiate()
-	add_cell(input_cell)
-	input_cell.set_preset(preset, preset_data)
-	
-	# Return for chaining if needed
-	return input_cell
+	for cell in get_children():
+		var cell_index = cell.get_index()
+		if len(input_rows) <= cell_index:
+			input_rows.resize(cell_index+1)
+			input_rows[-1] = InputRow.new()
+		input_rows[cell_index].add_cell(cell)
 
 # ====================================================== #
 #                      ROW EDITING                       #
