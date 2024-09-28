725392c9051c39942400eafe03394feed1fa3d3c
Rydahhhhhh <rmswerth@gmail.com>
2024-09-29 09:30:34 +1300

    Added documentation for the EasyGrid node class

--- Scripts/Global Nodes/EasyGrid.gd
+++ Scripts/Global Nodes/EasyGrid.gd
@@ -1,58 +1,67 @@
 @tool
 class_name EasyGrid extends HBoxContainer
 
-var grid_columns = []
+## A container that organizes its children into a pseudo-grid layout.
+##
+## A variant of HBoxContainer that organizes its children into a pseudo-grid layout. VBoxContainer children can be treated as "columns". Each column raises its child nodes to align with the highest child in any other column in the same row, creating a grid-like structure without explicitly defined rows.
+## 
+## @experimental
 
-func _get_configuration_warnings() -> PackedStringArray:
-	for node in grid_columns:
-		if node is not VBoxContainer:
-			return ["Nodes treated as grid must of VBoxContainer type"]
-	return []
+var grid_columns: Array[VBoxContainer] = []
 
+# ====================================================== #
+#                       OVERRIDES                        #
+# ====================================================== #
 func _notification(what: int) -> void:
 	match what:
-		NOTIFICATION_SORT_CHILDREN:
-			re_sort_children()
 		NOTIFICATION_THEME_CHANGED:
 			update_minimum_size()
 		NOTIFICATION_TRANSLATION_CHANGED, NOTIFICATION_LAYOUT_DIRECTION_CHANGED:
 			queue_sort()
+		NOTIFICATION_SORT_CHILDREN:
+			var row_minh = {}
+			
+			# Removing any freed columns that weren't removed from the array
+			for node in grid_columns.duplicate():
+				if not is_instance_valid(node):
+					grid_columns.erase(node)
+			
+			# I don't really know what happens when the children have expand
+			# modes that aren't the default, and I don't really want to make it
+			# work. I won't be doing that anyway - Rydahhhhh
+			for node: VBoxContainer in grid_columns:
+				for i in node.get_child_count():
+					var ms = node.get_child(i).get_minimum_size()
+					row_minh[i] = max(ms.y, row_minh.get(i, 0))
+			
+			for node: VBoxContainer in grid_columns:
+				for child in node.get_children():
+					child.custom_minimum_size.y = row_minh[child.get_index()]
 
-func is_config_correct():
-	update_configuration_warnings()
-	var warnings = _get_configuration_warnings()
-	if warnings:
-		if not Engine.is_editor_hint():
-			push_error(warnings[0])
-		return false
-	return true
-
-func re_sort_children():
-	if not is_config_correct():
-		return
+# ====================================================== #
+#                        METHODS                         #
+# ====================================================== #
+func add_grid_column(column: VBoxContainer) -> VBoxContainer:
+	if column not in grid_columns:
+		grid_columns.append(column)
 	
-	var row_minh = {}
+	if column.get_parent() == null:
+		# When the child is newly created
+		add_child(column, true)
+		column.owner = self
+	elif column.get_parent() != self:
+		column.reparent(self)
 	
-	for node: VBoxContainer in grid_columns:
-		for i in node.get_child_count():
-			var ms = node.get_child(i).get_minimum_size()
-			row_minh[i] = max(ms.y, row_minh.get(i, 0))
-	
-	for node: VBoxContainer in grid_columns:
-		for child in node.get_children():
-			child.custom_minimum_size.y = row_minh[child.get_index()]
-
-func add_grid_column(node: VBoxContainer):
-	grid_columns.append(node)
-	if node.get_parent() != self:
-		add_child(node, true)
-		node.owner = self
 	queue_sort()
-	return node
+	# Return for chaining
+	return column
 
-func remove_grid_column(node: VBoxContainer):
-	node.queue_free()
-	grid_columns.erase(node)
+func remove_grid_column(column: VBoxContainer) -> void:
+	# You can just free the node and it'll be handled fine
+	# but having a method that explicitly removes a column makes sense
+	column.queue_free()
+	grid_columns.erase(column)
+	return
 
 # ====================================================== #
 #                      END OF FILE                       #
