9de9e437d0bceb91689762883790b7c24395b0a1
Rydahhhhhh <rmswerth@gmail.com>
2024-10-01 13:02:50 +1300

    Check box input integrated delegate_property

--- Scenes/Character Planner Menu/check_box_input.gd
+++ Scenes/Character Planner Menu/check_box_input.gd
@@ -1,35 +1,15 @@
 @tool
 extends InputCell
 
-signal toggled(toggled_on: bool)
-
-@export var val: bool = false: set = _set_val, get = _get_val
 @onready var check_box: CheckBox = $CheckBox
 
 # ====================================================== #
 #                       OVERRIDES                        #
 # ====================================================== #
 func _ready() -> void:
+	check_box.toggled.connect(_on_value_changed)
+	delegate_property(check_box, "button_pressed", "value")
 	return
-
-# ====================================================== #
-#                    SETTER & GETTERS                    #
-# ====================================================== #
-func _set_val(to: bool): 
-	if not is_node_ready():
-		await ready
-	check_box.button_pressed = to
-
-func _get_val(): 
-	if not is_node_ready():
-		await ready
-	return check_box.button_pressed
-
-# ====================================================== #
-#                  SIGNAL CONNECTIONS                    #
-# ====================================================== #
-func _on_check_box_toggled(toggled_on: bool) -> void:
-	toggled.emit(toggled_on)
 
 # ====================================================== #
 #                      END OF FILE                       #
